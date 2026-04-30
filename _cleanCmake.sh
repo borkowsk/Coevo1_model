ef19b685105113ef26947748e50a402a5b4bafda
Wojciech T. Borkowski <wborkowsk@gmail.com>
2026-04-30 18:48:10 +0200

    Cmake tempraiers cleaner.

---
+++ _cleanCmake.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# @date 2026-04-29 (last update)
+
+if [ $# -ne 0 ]; 
+then
+   echo "No parameters expected!" 1>&2
+   exit -1
+fi
+
+echo "Cleaning cmake files!"
+echo "Cleaning current directoryfrom cmake files!" `date` >> clean_list.txt
+
+rm -f CMakeCache.txt
+rm -f CMakeLists.txt.user
+rm -f Makefile
+rm -f cmake_install.cmake
+rm -f *.cbp
+
+rm -Rf CMakeFiles/
+rm -Rf bin/CMakeFiles/
+rm -Rf cmake-build-*/
+
+
